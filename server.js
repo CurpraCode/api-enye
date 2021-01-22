e86087df9d9152c9eb77bb2e151341e1e98151c6
Oyakoya Curtis <40108842+Curtis-Oyakoya@users.noreply.github.com>
2021-01-22 23:07:37 +0100

    backend task done

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const axios = require("axios");
 const dotenv = require("dotenv").config({ path: "./config.env" });
 
-//Checking for uncaughtexception
 process.on("uncaughtException", (err) => {
   console.log("UNCAUGHT EXCEPTION!  Shutting down...");
   console.log(err.name, err.message);
